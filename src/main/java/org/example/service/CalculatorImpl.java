60549ca959a68058e08ec6121cbbd7d176d67eec
Pavel <pavel.filkovskiy@gmail.com>
2023-09-29 16:37:57 +0100

    refactor calculateTax() to make more performant

--- src/main/java/org/example/service/CalculatorImpl.java
+++ src/main/java/org/example/service/CalculatorImpl.java
@@ -4,6 +4,7 @@ import org.example.factory.SlabFactory;
 import org.example.pojo.Slab;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Objects;
 
 public class CalculatorImpl implements Calculator {
@@ -18,22 +19,30 @@ public class CalculatorImpl implements Calculator {
 
     @Override
     public double calculateTax(double income) {
-        if (Objects.isNull(income)) {
-            throw new RuntimeException("income can't be null!");
-        }
 
         if (income < 0.0) {
             throw new RuntimeException("income can't be less than 0!");
         }
 
-        return slabs.stream()
-                .filter(slab -> income >= slab.getMinthreshold())
-                .map(slab -> {
-                    if (income > slab.getMaxthreshold()) {
-                        return (slab.getMaxthreshold() - slab.getMinthreshold()) * slab.getTaxRate();
-                    }
-                    return (income - slab.getMinthreshold()) *  slab.getTaxRate();
-                })
-                .reduce(0.0, Double::sum);
+        double taxAmount = 0.0;
+        ListIterator<Slab> it = slabs.listIterator();
+
+        while(it.hasNext()) {
+            Slab slab = it.next();
+
+            if (income < slab.getMinthreshold()) {
+                break;
+            }
+
+            if (income > slab.getMaxthreshold()) {
+                taxAmount += (slab.getMaxthreshold() - slab.getMinthreshold()) * slab.getTaxRate();
+                continue;
+            }
+
+            taxAmount += (income - slab.getMinthreshold()) * slab.getTaxRate();
+        }
+
+        return taxAmount;
+
     }
 }
